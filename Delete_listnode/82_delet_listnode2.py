8bda552591a19fa6df58afceee077face2f0e93a
deathot <133761266+deathot@users.noreply.github.com>
2024-03-22 00:57:11 +0800

    Add files via upload

---
+++ Delete_listnode/82_delet_listnode2.py
@@ -0,0 +1,28 @@
+82_删除排序链表中的重复元素 II
+
+给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 
+
+时间复杂度o(n)
+空间复杂度o(1)
+
+总结:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(next = head)
+        cur = dummy
+        
+        while cur.next and cur.next.next:
+            val = cur.next.val
+            if cur.next.next.val == val:
+                while cur.next and cur.next.val == val:
+                        cur.next = cur.next.next
+            else:
+                cur = cur.next
+
+        return dummy.next
