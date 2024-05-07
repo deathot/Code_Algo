b0a9035f2b8fbd6c02633422400bf1caadcb0d13
deathot <133761266+deathot@users.noreply.github.com>
2024-05-08 00:46:37 +0800

    Update 494_Target_sum.py

--- Dp_Alo/0-1Bag&All_bag/494_Target_sum.py
+++ Dp_Alo/0-1Bag&All_bag/494_Target_sum.py
@@ -50,3 +50,31 @@ class Solution:
             return dfs(i - 1, c) + dfs(i - 1, c - nums[i])
 
         return dfs(n - 1, target)
+
+# 法二 记忆化 -> 递推
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        '''
+        + = p
+        - = s - p
+        t = + - - -> p - (s - p) = 2p - s
+        p = (t + s) // 2
+        
+        '''
+        target += sum(nums)
+        if target < 0 or target % 2 != 0:
+            return 0
+        target //= 2
+        n = len(nums)
+
+        f = [[0] * (target + 1) for _ in range(n + 1)]
+        f[0][0] = 1
+        
+        for i, x in enumerate(nums):
+            for c in range(target + 1):
+                if c < x:
+                    f[i + 1][c] = f[i][c]
+                else:
+                    f[i + 1][c] = f[i][c] + f[i][c - x]
+        
+        return f[n][target]
